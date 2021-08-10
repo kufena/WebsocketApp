88812ad4170afa0e94117d03916718dc19f00bf4
Andrew Douglas <potterboy67@outlook.com>
2021-08-10 21:52:18 +0100

    Well, you can do multiple clients and it passes messages back and forth, but it is not robust.

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -11,46 +11,80 @@ namespace ConsoleClient
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Worker wk = new Worker();
+            wk.init();
 
-            using (ClientWebSocket client = new ClientWebSocket())
+            wk.DoWait();
+
+            var th = new Thread(wk.DoListen);
+            th.Start();
+            await wk.DoSend();
+            th.Join();
+        }
+    }
+
+    public class Worker
+    {
+        ClientWebSocket client;
+
+        public Worker()
+        {
+        }
+
+        public async void init() {
+            Uri serviceURI = new Uri("ws://localhost:5000/send");
+            var cTs = new CancellationTokenSource();
+            cTs.CancelAfter(TimeSpan.FromSeconds(120));
+
+            client = new ClientWebSocket();
+            await client.ConnectAsync(serviceURI, cTs.Token);
+        }
+
+        public async void DoListen()
+        {
+            var cTs = new CancellationTokenSource();
+            cTs.CancelAfter(TimeSpan.FromSeconds(120));
+
+            while (client.State == WebSocketState.Open)
             {
-                Uri serviceURI = new Uri("ws://localhost:5000/send");
-                var cTs = new CancellationTokenSource();
-                cTs.CancelAfter(TimeSpan.FromSeconds(120));
-                try
-                {
-                    await client.ConnectAsync(serviceURI, cTs.Token);
-                    var n = 0;
-                    while (client.State == WebSocketState.Open)
-                    {
-                        Console.WriteLine("enter message:");
-                        string s = Console.ReadLine();
+                Console.WriteLine("enter message:");
+                string s = Console.ReadLine();
 
-                        await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(s)), 
-                            WebSocketMessageType.Text, 
-                            true,
-                            cTs.Token);
-                        var responseBuffer = new byte[1024];
-                        var offset = 0;
-                        var packet = 1024;
-                        while(true)
-                        {
-                            var byteReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
-                            var response = await client.ReceiveAsync(byteReceived, cTs.Token);
-                            var responseMsg = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
-                            Console.WriteLine(responseMsg);
+                await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(s)),
+                    WebSocketMessageType.Text,
+                    true,
+                    cTs.Token);
+            }
+            Console.WriteLine("Dropping out of Listen.");
+        }
 
-                            if (response.EndOfMessage)
-                                break;
-                        }
-                    }
-                }
-                catch (WebSocketException ex)
+        public async Task DoSend()
+        {
+
+            var cTs = new CancellationTokenSource();
+            cTs.CancelAfter(TimeSpan.FromSeconds(120));
+
+            while (client.State == WebSocketState.Open)
+            {
+                var responseBuffer = new byte[1024];
+                var offset = 0;
+                var packet = 1024;
+                while (true)
                 {
-                    Console.WriteLine(ex.Message);
+                    var byteReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
+                    var response = await client.ReceiveAsync(byteReceived, cTs.Token);
+                    var responseMsg = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                    Console.WriteLine(responseMsg);
+
+                    if (response.EndOfMessage)
+                        break;
                 }
             }
+        }
+
+        internal void DoWait()
+        {
+            while (client.State != WebSocketState.Open) ;
         }
     }
 }
